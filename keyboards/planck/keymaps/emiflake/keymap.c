85427e32d93ad50d77a739c8169c23008e5c42de
Emily Martins <emily.flakeheart@gmail.com>
2020-02-21 15:43:56 +0100

    remove redundant crud

--- keyboards/planck/keymaps/emiflake/keymap.c
+++ keyboards/planck/keymaps/emiflake/keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2015-2017 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "planck.h"
 #include "action_layer.h"
 
@@ -50,15 +34,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * |      | Ctrl | Alt  | GUI  |Lower |      | Space|Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] =
 {
- {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
- {KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH},
- {KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT },
- {BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+ { KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,  KC_Y,    KC_F,   KC_G,  KC_C,    KC_R,    KC_L,  KC_BSPC },
+ { KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,  KC_I,    KC_D,   KC_H,  KC_T,    KC_N,    KC_S,  KC_SLSH },
+ { KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,  KC_X,    KC_B,   KC_M,  KC_W,    KC_V,    KC_Z,  KC_ENT  },
+ { _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER, _______, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT }
 },
 
 
@@ -75,10 +59,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_GAME] =
 {
- {KC_1,  KC_TAB,    KC_Q,    KC_W,  KC_E, KC_R,   KC_T,   KC_Y,  KC_U,    KC_UP,   KC_O,     KC_P},
- {KC_2,  KC_ESC,    KC_A,    KC_S,  KC_D, KC_F,   KC_G,   KC_H,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_SCLN},
- {KC_3, KC_LSFT,    KC_Z,    KC_X,  KC_C, KC_V,   KC_B,   KC_N,  KC_M,    KC_COMM, KC_DOT,   KC_ENT },
- {KC_4, KC_LCTL, _______, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, _______, _______, _______,  G_EXIT }
+ { KC_1,  KC_TAB, KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,   KC_Y,  KC_U,    KC_UP,   KC_O,     KC_P    },
+ { KC_2,  KC_ESC, KC_A,    KC_S,    KC_D,  KC_F,   KC_G,   KC_H,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_SCLN },
+ { KC_3, KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,   KC_N,  KC_M,    KC_COMM, KC_DOT,   KC_ENT  },
+ { KC_4, KC_LCTL, _______, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, _______, _______, _______,  G_EXIT  }
 },
 
 /* Raise
@@ -94,10 +78,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] =
 {
- {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR, KC_UNDS,  KC_PIPE, KC_BSPC},
- {_______, _______, _______, KC_EQL,  KC_LPRN,    KC_LBRC, KC_RBRC, KC_RPRN,   KC_PLUS, KC_MINUS, KC_BSLS, KC_HOME},
- {_______, _______, _______, _______, S(KC_COMM), KC_LCBR, KC_RCBR, S(KC_DOT), _______, _______,  _______, _______},
- {_______, _______, _______, _______, _______,    _______, _______, _______,   _______, _______,  _______, _______}
+ { KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR, KC_UNDS,  KC_PIPE, KC_BSPC },
+ { _______, _______, _______, KC_EQL,  KC_LPRN,    KC_LBRC, KC_RBRC, KC_RPRN,   KC_PLUS, KC_MINUS, KC_BSLS, KC_HOME },
+ { _______, _______, _______, _______, S(KC_COMM), KC_LCBR, KC_RCBR, S(KC_DOT), _______, _______,  _______, _______ },
+ { _______, _______, _______, _______, _______,    _______, _______, _______,   _______, _______,  _______, _______ }
 },
 
 /* Lower
@@ -113,10 +97,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] =
 {
- {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC},
- {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______},
- {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______,  _______},
- {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,  KC_MPLY}
+ { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC },
+ { KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______ },
+ { _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______,  _______ },
+ { _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU,  KC_MPLY }
 },
 
 /* Plover layer (http://opensteno.org)
@@ -133,17 +117,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_PLOVER] =
 {
- {KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1   },
- {XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC},
- {XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
- {EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX}
+ { KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1,    KC_1    },
+ { XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC },
+ { XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
+ { EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX }
 },
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * |      | Reset|      |  M^  |      |      |      |  M1  |  M2  |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |AGnorm|AGswap|      | Game |Dvorak|Plover|      |
+ * |      |      |  M<  |  Mv  |  M>  |AGnorm|AGswap|      | Game |Dvorak|Plover|      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -152,10 +136,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =
 {
- {_______, RESET,   _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______},
- {_______, TERM_ON, TERM_OFF, _______, _______, AG_NORM, AG_SWAP, _______, GAME,    DVORAK,  PLOVER,  _______},
- {_______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______},
- {_______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______}
+ { _______, RESET,   _______,    KC_MS_UP,   _______,     _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______ },
+ { _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, AG_NORM, AG_SWAP, _______, GAME,    DVORAK,  PLOVER,  _______ },
+ { _______, _______, _______,    _______,    _______,     _______, _______, _______, _______, _______, _______, _______ },
+ { _______, _______, _______,    _______,    _______,     _______, _______, _______, _______, _______, _______, _______ }
 }
 
 
